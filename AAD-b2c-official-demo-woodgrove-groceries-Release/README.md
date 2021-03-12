e47de07841b0fe34198df790dcec3953e88dffa3
YajZhang <yajzhan@microsoft.com>
2021-03-12 17:36:50 +0800

    new demo

---
+++ AAD-b2c-official-demo-woodgrove-groceries-Release/README.md
@@ -0,0 +1,5 @@
+# Woodgrove Groceries Demo
+
+This repository includes all the relevant source code and policies for the demo site: https://aka.ms/CIAMDemo
+
+You can configure the demo here:  https://aka.ms/CIAMDemoConfig
---
+++ AAD-b2c-official-demo-woodgrove-groceries-Release/src/WoodGroveGroceriesWebApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace WoodGroveGroceriesWebApplication
+{
+    using EntityFramework;
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
+    using Models.Settings;
+    using Services;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = BuildWebHost(args);
+
+            using (var serviceScope = webHost.Services.CreateScope())
+            {
+                var serviceProvider = serviceScope.ServiceProvider;
+                var dbContext = serviceProvider.GetRequiredService<WoodGroveGroceriesDbContext>();
+                var dbContextInitializationOptions = serviceProvider.GetRequiredService<IOptions<DbContextInitializationOptions>>().Value;
+                var host = serviceProvider.GetRequiredService<HostService>();
+
+                WoodGroveGroceriesDbContextInitializer.InitializeAsync(dbContext, dbContextInitializationOptions, host)
+                    .Wait();
+            }
+
+            webHost.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
